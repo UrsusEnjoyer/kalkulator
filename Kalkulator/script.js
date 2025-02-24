58f5394777405dd923a4d5bf70206e2f9d8bd8b0
Franciszek Łabuz <labuz.franciszek291@gmail.com>
2025-02-24 19:53:44 +0100

    Add files via upload

---
+++ Kalkulator/script.js
@@ -0,0 +1,22 @@
+// KALKULATOR
+
+const display = document.getElementById("display");
+
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+
+function clearDisplay() {
+    display.value = "";
+}
+
+
+function Calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error"; 
+    }
+}
